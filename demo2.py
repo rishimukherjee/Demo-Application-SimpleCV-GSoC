ff2ab85fa6981cf9745820c20fe7152bdde24e62
Rishi Mukherjee <thethunder666@gmail.com>
2012-04-05 12:24:12 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -5,13 +5,13 @@ import numpy as np
 
 img1 = Image("s.jpg")
 img2 = Image("t.jpg")
-src = img1.copy()
-dst = Image((1600, 1200))
+dst = Image((2000, 1600))
+dst = dst.blit(img1)
 ofimg = img1.findKeypointMatch(img2)
 homo = ofimg[1]
 eh = dst.getMatrix()
-j = cv2.warpPerspective(np.array((img2.getMatrix())), homo, (eh.rows, eh.cols), np.array(eh), cv.INTER_CUBIC)
-poster = Image((3200, 1600))
-poster = poster.blit(img1, pos=(0, 250))
-poster = poster.blit(Image(j), alpha=0.3, pos=(250, 10))
-poster.save("trying.jpg")
+x = Image(cv2.warpPerspective(np.array((img2.getMatrix())), homo, (eh.rows, eh.cols+300), np.array(eh), cv.INTER_CUBIC))
+x = x.blit(img1, alpha=0.4)
+x.save("rishi1.jpg")
+
+##visit http://imgur.com/a/lrGw4#0 to view the results.
