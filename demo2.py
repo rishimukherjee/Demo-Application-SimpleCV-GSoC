cf1e15d1b85d5cc47fabad35dec2e557d7274167
Rishi Mukherjee <thethunder666@gmail.com>
2012-04-10 10:36:10 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -4,12 +4,12 @@ from SimpleCV import *
 img1 = Image("s.jpg")
 img2 = Image("t.jpg")
 dst = Image((2000, 1600))
-dst = dst.blit(img1)
-ofimg = img1.findKeypointMatch(img2)
-homo = ofimg[1]
+ofimg = img1.findKeypointMatch(img2) #find the keypoints
+homo = ofimg[1] #the homography matrix
 eh = dst.getMatrix()
+#transform the image
 x = Image(cv2.warpPerspective(np.array((img2.getMatrix())), homo, (eh.rows, eh.cols+300), np.array(eh), cv.INTER_CUBIC))
+#blit the img1 now on coordinate (0, 0)
 x = x.blit(img1, alpha=0.4)
 x.save("rishi1.jpg")
-
 ##visit http://imgur.com/a/lrGw4#0 to view the results.
