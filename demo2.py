018fc0449f8d2a00666db67c9a79d06aead47307
Rishi Mukherjee <thethunder666@gmail.com>
2012-04-18 22:09:36 +0530

    Color problem solved.

--- demo2.py
+++ demo2.py
@@ -8,8 +8,8 @@ ofimg = img1.findKeypointMatch(img2) #find the keypoints
 homo = ofimg[1] #the homography matrix
 eh = dst.getMatrix()
 #transform the image
-x = Image(cv2.warpPerspective(np.array((img2.getMatrix())), homo, (eh.rows, eh.cols+300), np.array(eh), cv.INTER_CUBIC))
+x = Image(cv2.warpPerspective(np.array((img2.getMatrix())), homo, (eh.rows, eh.cols+300), np.array(eh), cv.INTER_CUBIC), colorSpace=ColorSpace.RGB).toBGR()
 #blit the img1 now on coordinate (0, 0)
 x = x.blit(img1, alpha=0.4)
 x.save("rishi1.jpg")
-##visit http://imgur.com/a/lrGw4#0 to view the results.
+##visit http://imgur.com/Xf7WS to view the results.
