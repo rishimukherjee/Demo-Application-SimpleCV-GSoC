9f2c913d9eec6ea1cdfbe94b26b87c3c14ffee03
Rishi Mukherjee <thethunder666@gmail.com>
2012-04-18 22:27:10 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -12,6 +12,6 @@ eh = dst.getMatrix()
 #transform the image
 x = Image(cv2.warpPerspective(np.array((img2.getMatrix())), homo, (eh.rows, eh.cols+300), np.array(eh), cv.INTER_CUBIC), colorSpace=ColorSpace.RGB).toBGR()
 #blit the img1 now on coordinate (0, 0)
-x = x.blit(img1, alpha=0.4)
+x = x.blit(img1, alpha=1.0)
 x.save("rishi1.jpg")
 ##visit http://imgur.com/Xf7WS to view the results.
